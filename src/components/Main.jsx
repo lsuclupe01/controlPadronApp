b2cca945f588a9050ce650f1730e3c0c8bc0818c
lsuclupe <lsuclupe@gitlab.com>
2024-04-22 16:17:04 -0500

    Control padrón app

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text,View} from 'react-native';
+import RepositoryList from "./RepositoryList";
+import AppBar from "./AppBar";
+import { Route, Routes, BrowserRouter,Navigate } from "react-router-native";
+
+const Main = () =>{
+    return(
+        <View style={{flex:1}}>            
+            <AppBar/>           
+            <Routes>
+                <Route path="/" element={<RepositoryList/>} />                                    
+                <Route path="/signin" element={<Text>Working on it</Text>}/>      
+            </Routes>
+           
+        </View>
+    )
+}
+
+export default Main
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Image, View, StyleSheet,Text } from 'react-native'
+import StyledText from './StyledText.jsx'
+import RepositoryStats from './RepositoryStats.jsx'
+import theme from '../theme.js'
+
+const RepositoryItemHeader = ({ ownerAvatarUrl, fullName, description, language, equipo }) => (
+  <View style={{ flexDirection: 'row', paddingBottom: 2 }}>
+    <View style={{ paddingRight: 10 }}>
+      <Image style={styles.image} source={{ uri: ownerAvatarUrl }} />
+    </View>
+    <View style={{ flex: 1 }}>
+      <StyledText fontWeight='bold'>{fullName}</StyledText>
+      <StyledText color='secondary'>{description}</StyledText>            
+      <View style={{ flexDirection: 'row', paddingBottom: 2 }}>
+        <StyledText style={styles.language}>{language}</StyledText>
+        <Text>   </Text>
+        <StyledText style={styles.equipo}>{equipo}</StyledText>
+      </View>
+    </View>
+  </View>
+)
+
+const RepositoryItem = (props) => (
+  <View key={props.id} style={styles.container}>
+    <RepositoryItemHeader {...props} />   
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingVertical: 5
+  },
+  language: {
+    padding: 4,
+    color: theme.colors.white,
+    backgroundColor: theme.colors.primary,
+    alignSelf: 'flex-start',
+    marginVertical: 4,
+    borderRadius: 4,
+    overflow: 'hidden'
+  },
+  equipo: {
+    padding: 4,
+    color: theme.colors.white,
+    backgroundColor: theme.colors.azulMarino,
+    alignSelf: 'flex-start',
+    marginVertical: 4,
+    borderRadius: 4,
+    overflow: 'hidden'
+  },
+  image: {
+    width: 48,
+    height: 48,
+    borderRadius: 4
+  }
+})
+
+export default RepositoryItem
